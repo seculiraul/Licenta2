95e3840d90b812bb1809a4aff0af64557aa09999
reched <darius.rechesan@cpp.canon>
2022-06-09 21:05:46 +0300

    added products,images for them & design changes

--- src/app/produs.model.ts
+++ src/app/produs.model.ts
@@ -2,7 +2,8 @@ export interface Produs {
     id: string,
     nume: string,
     img: string,
-    pret: number,
+    pret: string,
     categorie: string,
+    descriere: string,
     isNew: boolean
 }
